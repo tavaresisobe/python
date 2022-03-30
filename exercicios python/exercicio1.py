0bf3cf30b08b03b57c0acfc8e9c1ed16452eaa2e
Gustavo Henrique Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2022-03-30 19:52:30 -0300

    Add files via upload

---
+++ exercicios python/exercicio1.py
@@ -0,0 +1,10 @@
+nome = input("Digite o nome do aluno: ")
+
+nota1 = input("Digite a primeira nota do aluno: ")
+nota2 = input("Digite a segunda nota do aluno: ")
+nota3 = input("Digite a terceira nota do aluno: ")
+nota4 = input("Digite a quarta nota do aluno: ")
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+print ("O aluno " + nome + " ficou com a média", media)
---
+++ exercicios python/exercicio5.py
@@ -0,0 +1,5 @@
+leia = input ("Digite uma frase: ")
+cont = len(leia)
+frase = f"A frase digitada possui %d caracteres" % cont
+
+print (frase)
