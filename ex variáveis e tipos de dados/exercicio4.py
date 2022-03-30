f237834c817b70102995e48ceda32a0b94dba282
Gustavo Henrique Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2022-03-30 19:58:13 -0300

    Add files via upload

---
+++ ex variáveis e tipos de dados/exercicio4.py
@@ -0,0 +1,9 @@
+a = int(input("Informe o valor de a: "))
+b = int(input("Informe o valor de b: "))
+maior = (a > b)
+menor = (a < b)
+igual = (a == b)
+
+print("a é maior que b:",maior)
+print("a é menor que b:",menor)
+print("a é igual que b:",igual)
