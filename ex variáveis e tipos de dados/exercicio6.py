4166236433766f74304238918b722c4c41b27d20
Gustavo Henrique Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2022-03-31 19:57:15 -0300

    Update exercicio6.py

--- ex variáveis e tipos de dados/exercicio6.py
+++ ex variáveis e tipos de dados/exercicio6.py
@@ -1,14 +1,17 @@
-cpf = input("Digite seu cpf com pontuação: ")
-antes = cpf[0] + cpf[1] + cpf[2] + cpf[3] + cpf[4] + cpf[5]
-+ cpf[6] + cpf[7] + cpf[8] + cpf[9] + cpf[10]
-dhifen = cpf[12] + cpf[13]
+import os
 
-parte1 = cpf[0]+ cpf[1] + cpf[2]
-parte2 = cpf[4]+ cpf[5] + cpf[6]
-parte3 = cpf[8]+ cpf[9] + cpf[10]
+cpf = input("Digite seu cpf com pontuação: ")
+dhifen = cpf[12:14]
+antes = cpf[0:11]
+
+parte1 = cpf[0:3]
+parte2 = cpf[4:7]
+parte3 = cpf[8:11]
 
 print ("Antes do hífen:", antes)
 print ("Depois do hífen:", dhifen)
 print ("Parte 1:", parte1)
 print ("Parte 2:", parte2)
 print ("Parte 3:", parte3)
+
+os.system("PAUSE")
