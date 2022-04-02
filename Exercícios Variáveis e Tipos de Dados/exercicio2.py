adcc7853e29310c381bf2aa533e4c7fcfe7e9883
Gustavo Henrique Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2022-04-01 21:49:21 -0300

    Add files via upload

---
+++ Exercícios Variáveis e Tipos de Dados/exercicio2.py
@@ -0,0 +1,10 @@
+import os
+
+v_compra = float(input("Informe o valor da compra: "))
+parcelas = int(input("Informe o número de parcelas: "))
+v_parcelas = v_compra / parcelas
+frase = f"O valor da compra foi de %f, o número de parcelas informado foi %d e o valor da parcela é %.3f" % (v_compra, parcelas, v_parcelas)
+
+print (frase)
+
+os.system("PAUSE")
