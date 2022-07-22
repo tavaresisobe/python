9b15ef8e8d10c3f1007cd298e5187d1c80c14558
Gustavo Henrique Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2022-07-21 21:54:22 -0300

    Update compararstrings.py

--- Desafios Python/Comparar Strings/compararstrings.py
+++ Desafios Python/Comparar Strings/compararstrings.py
@@ -1,19 +1,11 @@
 import os
 
-text = input ("Digite a primeira string: ")
-text2 = input ("Digite a segunda string: ")
+string_1 = input('Digite a primeira string: ')
+string_2 = input('Digite a segunda string: ')
 
-cont = len(text)
-cont2 = len(text2)
+print ("Quantidade de caracteres de '{}': {}".format(string_1, len(string_1)))
+print ("Quantidade de caracteres de '{}': {}".format(string_2, len(string_2)))
+print ('As strings possuem a mesma quantidade de caracteres ?', len (string_1) == len (string_2))
+print ("Os textos são iguais ?", string_1 == string_2)
 
-print ("Quantidade de caracteres de " + text + " é:", cont)
-print ("Quantidade de caracteres de " + text2 + " é:", cont2)
-
-check = (cont == cont2)
-check2 = (text == text2)
-
-print ("As strings possuem a mesma quantidade de caracteres:", check)
-print ("As strings são iguais:", check2)
-    
-
-os.system("PAUSE")
+os.system("pause")
