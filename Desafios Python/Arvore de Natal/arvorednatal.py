7c37409f16d0fb692a756f6750914bd687412c6b
Gustavo Henrique Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2022-07-21 22:05:34 -0300

    Add files via upload

---
+++ Desafios Python/Arvore de Natal/arvorednatal.py
@@ -0,0 +1,17 @@
+import os
+
+arvore = '*'
+print (" "*10 + arvore + " "*10)
+print (" "*9 + arvore * 3 + " "*9)
+print (" "*8 + arvore * 5 + " "*8)
+print (" "*7 + arvore * 7 + " "*7)
+print (" "*6 + arvore * 9 + " "*6)
+print (" "*5 + arvore * 11 + " "*5)
+print (" "*4 + arvore * 13 + " "*4)
+print (" "*3 + arvore * 15 + " "*3)
+print (" "*2 + arvore * 17 + " "*2)
+print (" " + arvore * 19 + " ")
+print (" "*9 + arvore * 3 + " "*9)
+print (" "*9 + arvore * 3 + " "*9)
+
+os.system("pause")
