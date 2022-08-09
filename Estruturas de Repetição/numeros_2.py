8676a8b2816bdf30c1c8f6f36345f5cf63f9d283
Gustavo Henrique Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2022-08-09 20:42:58 -0300

    Add files via upload

---
+++ Estruturas de Repetição/numeros_2.py
@@ -0,0 +1,4 @@
+str = ""
+for i in range(1,10,1):
+  str = f"{i}"
+  print (str * i)
---
+++ Estruturas de Repetição/reverso.py
@@ -0,0 +1,5 @@
+word = (input("Digite uma palavra: "))
+r_word = ""
+for i in word:
+ r_word = i + r_word
+print(r_word)
